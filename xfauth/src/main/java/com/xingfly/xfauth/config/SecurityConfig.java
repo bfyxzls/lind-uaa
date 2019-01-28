3d5a7874194b94c2706811d1a432bf8f6afc728e
zhangzl <zhangzl@i-counting.cn>
2019-01-28 10:36:42 +0800

    修改mysql密码，添加注释

--- xfauth/src/main/java/com/xingfly/xfauth/config/SecurityConfig.java
+++ xfauth/src/main/java/com/xingfly/xfauth/config/SecurityConfig.java
@@ -20,34 +20,45 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private JPAUserDetailsService jpaUserDetailsService;
+  @Autowired
+  private JPAUserDetailsService jpaUserDetailsService;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http.authorizeRequests().
-//                antMatchers("/test").permitAll().
-                anyRequest().authenticated()
-                .and()
-                .csrf().disable();
-        // @formatter:on
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // ingore是完全绕过了spring security的所有filter，相当于不走spring security
+    // permitAll没有绕过spring security，其中包含了登录的以及匿名的。
+    http.authorizeRequests()
+        .antMatchers("/test").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .csrf().disable();
+  }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(jpaUserDetailsService).passwordEncoder(passwordEncoder());
-    }
+  /**
+   * 密码加密的实现.
+   *
+   * @param auth .
+   * @throws Exception .
+   */
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(jpaUserDetailsService).passwordEncoder(passwordEncoder());
+  }
 
-    //不定义没有password grant_type
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+  /**
+   * 不定义没有password grant_type
+   *
+   * @return .
+   * @throws Exception .
+   */
+  @Override
+  @Bean
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
 }
